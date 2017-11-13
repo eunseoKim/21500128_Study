fa39d2001af4f794acd4cbe05ca120c8119cd1ba
eunseo kim <21500128@handong.edu>
2017-11-13 16:49:02 +0000

    reference

--- practice02.cpp
+++ practice02.cpp
@@ -2,7 +2,17 @@
 
 int main() //NULL pointer practice
 {
-    int *ptr = NULL;
-    std::cout << "The value of ptr is" << ptr;
+    int *ptr = NULL;//pointer can have null, but reference can't have null!
+    std::cout << "The value of ptr is" << ptr << std::endl;
     
+    int var1;
+    char var2[10];
+    
+    std::cout << "Address of var1 variable: ";
+    std::cout << &var2 << std::endl;//& is reference
+    
+    std::cout << "Address of var2 variable: ";
+    std::cout << &var2 << std::endl;
+    
+    return 0;
 }
