2ae4ab07b3045762fc5ff52c095ea0ed3cc5bee5
eunseo kim <21500128@handong.edu>
2017-11-22 16:08:07 +0000

    int Human

--- practice04.cpp
+++ practice04.cpp
@@ -21,3 +21,32 @@ private:
     void Aging();
     void Growing();
 };
+
+int Human::GetAge()
+{
+    return age;
+}
+
+char* Human::GetName()
+{
+    return name;
+}
+
+void Human::AskName(Human other_person){
+    cout << GetName() << ":" << "Hi, what is your name?" << endl;
+    other_person.What_is_your_name();
+}
+
+void Human::AskAge(Human other_person){
+    cout << GetName() << ":" << "How old are you?" << endl;
+    other_person.How_old_are_you();
+}
+
+void Human::What_is_your_name(){
+    cout << GetName() << ":" << "Hello, I am" << GetName() << endl;
+}
+
+void Human::How_old_are_you(){
+    cout << GetName() << ":" << "I am" << GetAge() << "."  <<endl;
+}
+
